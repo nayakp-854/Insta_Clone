98ab286ed1773284b6a483b78942b440f99f3026
PRINCE NAYAK <113225737+nayakp-854@users.noreply.github.com>
2024-03-22 12:31:20 +0530

    second-1

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -41,9 +41,56 @@ export default function Navbar({ login }) {
     }
   };
 
+  const loginStatusMobile = () => {
+    const token = localStorage.getItem("jwt");
+    if (login || token) {
+      return [
+        <>
+          <Link to="/">
+            <li>
+              <span class="material-symbols-outlined">home</span>
+            </li>
+          </Link>
+          <Link to="/profile">
+            <li>
+              <span class="material-symbols-outlined">account_circle</span>
+            </li>
+          </Link>
+          <Link to="/createPost">
+            <li>
+              <span class="material-symbols-outlined">add_box</span>
+            </li>
+          </Link>
+          <Link style={{ marginLeft: "20px" }} to="/followingpost">
+            <li>
+              <span class="material-symbols-outlined">explore</span>
+            </li>
+          </Link>
+          <Link to={""}>
+            <li onClick={() => setModalOpen(true)}>
+              <span class="material-symbols-outlined">logout</span>
+            </li>
+          </Link>
+        </>,
+      ];
+    } else {
+      return [
+        <>
+          <Link to="/signup">
+            <li>SignUp</li>
+          </Link>
+          <Link to="/signin">
+            <li>SignIn</li>
+          </Link>
+        </>,
+      ];
+    }
+  };
+
   return (
     <div className="navbar">
       <img
+        id="insta-logo"
         src={logo}
         alt="this is logo"
         onClick={() => {
@@ -52,6 +99,7 @@ export default function Navbar({ login }) {
         style={{ cursor: "pointer" }}
       />
       <ul className="nav-menu">{loginStatus()}</ul>
+      <ul className="nav-mobile">{loginStatusMobile()}</ul>
     </div>
   );
 }
